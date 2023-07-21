ce78f30de7455a9ed3aaa7399112264a8467ead7
Omar muhamamd <Omarfouad15e@gmail.com>
2023-07-22 00:05:37 +0300

    vote up and down

--- server/models/mongo.config.js
+++ server/models/mongo.config.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-
-const mongodbUrl = 'mongodb://omarfouad15e:Omarfouad123@ac-x7p6sxs-shard-00-00.ak7vqjm.mongodb.net:27017,ac-x7p6sxs-shard-00-01.ak7vqjm.mongodb.net:27017,ac-x7p6sxs-shard-00-02.ak7vqjm.mongodb.net:27017/?ssl=true&replicaSet=atlas-v2i4l2-shard-0&authSource=admin&retryWrites=true&w=majority'; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
+; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
 
 if (!mongodbUrl) {
   console.log('\x1b[33m%s\x1b[0m','Please set the mongodb connection first in -> "server/models/mongo.config.js"\n');
