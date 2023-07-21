3bcb9f5ef74cf4acecd25080a9ca874e224a14bd
Omar muhamamd <Omarfouad15e@gmail.com>
2023-07-22 00:02:49 +0300

    vote up and down

--- server/models/mongo.config.js
+++ server/models/mongo.config.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-const mongodbUrl = ''; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
+const mongodbUrl = 'mongodb://omarfouad15e:Omarfouad123@ac-x7p6sxs-shard-00-00.ak7vqjm.mongodb.net:27017,ac-x7p6sxs-shard-00-01.ak7vqjm.mongodb.net:27017,ac-x7p6sxs-shard-00-02.ak7vqjm.mongodb.net:27017/?ssl=true&replicaSet=atlas-v2i4l2-shard-0&authSource=admin&retryWrites=true&w=majority'; // TODO: PUT YOUR VALID MONGODB CONNECTION URL HERE <-
 
 if (!mongodbUrl) {
   console.log('\x1b[33m%s\x1b[0m','Please set the mongodb connection first in -> "server/models/mongo.config.js"\n');
   return;
 }
 
-mongoose.connect(mongodbUrl);
+mongoose.connect(mongodbUrl,{useUnifiedTopology:true,  useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
