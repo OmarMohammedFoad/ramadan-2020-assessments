eb0759a1b1ef5dedafd69a962221ac9aeb122263
Omar muhamamd <Omarfouad15e@gmail.com>
2023-07-22 11:55:13 +0300

    Sortedbyadded

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 
 
-function getdata(element)
+const listofconatiers = document.getElementById("listOfRequests");
+
+function getdata(element,flag=false)
 {
     const makediv = document.createElement("div");
         makediv.innerHTML = ` <div class="card mb-3">
@@ -9,7 +11,7 @@ function getdata(element)
             <h3>${element.topic_title}</h3>
             <p class="text-muted mb-2">${element.author_name}</p>
             <p class="mb-0 text-muted">
-              <strong>Expected results : </strong>${element.expected_result}
+             ${  element.expected_result &&`<strong>Expected results : </strong>${element.expected_result}`}
             </p>
           </div>
           <div class="d-flex flex-column text-center">
@@ -33,42 +35,70 @@ function getdata(element)
           </div>
         </div>
         </div>`
-        ;
-        return makediv;
-}
 
+        if(flag==true){
+        listofconatiers.prepend(makediv);
+        }
+        else
+        {
+          listofconatiers.appendChild(makediv);
+        }
+        
+        const votes_up_elm = document.getElementById(`votes_up_${element._id}`);
+        const score_elm = document.getElementById(`score_${element._id}`);
+        const votes_down_elm = document.getElementById(`votes_down_${element._id}`) 
+        
+        votes_up_elm.addEventListener('click',(e)=>{
+          fetch("http://localhost:7777/video-request/vote",{method:'PUT',
+          headers:{'content-Type':'application/json'},
+          body:JSON.stringify({id:element._id,vote_type:"ups"})},
+          ).then(bold=>bold.json()).then(data=>score_elm.innerText = data.votes.ups-data.votes.downs)
+        })
+        
+        votes_down_elm.addEventListener('click',(e)=>{
+          fetch("http://localhost:7777/video-request/vote",{method:'PUT',
+          headers:{'content-Type':'application/json'},
+          body:JSON.stringify({id:element._id,vote_type:"downs"})},
+          ).then(bold=>bold.json()).then(data=>score_elm.innerText = data.votes.ups-data.votes.downs)
+        });
+        
+      }
+
+
+
+      
 document.addEventListener("DOMContentLoaded",function()
       {
-        const listofconatiers = document.getElementById("listOfRequests");
-    
+        
+
+        const sortElems = document.querySelectorAll('[id*=sort_by_]');
+
+        sortElems.forEach((elm)=>{
+          elm.addEventListener('click',function(e)
+          {
+            e.preventDefault();
+            const sortby = this.querySelector("input");
+            fetch(`http://localhost:7777/video-request?sortby=${sortby.value}`).then((bold)=>bold.json()).then((data)=>{
+            listofconatiers.innerHTML = '';
+            data.forEach(element=>
+            getdata(element))
+          })
+          })
+        })
+
+
+
         fetch("http://localhost:7777/video-request").then(bold=>bold.json())
         .then((data)=>
         {
-            
+
             data.forEach(element=>
                {
             
                 
-            listofconatiers.appendChild(getdata(element));
+                  getdata(element);
             
-            const votes_up_elm = document.getElementById(`votes_up_${element._id}`);
-            const score_elm = document.getElementById(`score_${element._id}`);
-            const votes_down_elm = document.getElementById(`votes_down_${element._id}`) 
-
-            votes_up_elm.addEventListener('click',(e)=>{
-                fetch("http://localhost:7777/video-request/vote",{method:'PUT',
-                headers:{'content-Type':'application/json'},
-                body:JSON.stringify({id:element._id,vote_type:"ups"})},
-                ).then(bold=>bold.json()).then(data=>score_elm.innerText = data.votes.ups-data.votes.downs)
-            })
-
-            votes_down_elm.addEventListener('click',(e)=>{
-                fetch("http://localhost:7777/video-request/vote",{method:'PUT',
-                headers:{'content-Type':'application/json'},
-                body:JSON.stringify({id:element._id,vote_type:"downs"})},
-                ).then(bold=>bold.json()).then(data=>score_elm.innerText = data.votes.ups-data.votes.downs)
-            })
-            
+           
                })
         }
         );
@@ -90,10 +120,10 @@ document.addEventListener("DOMContentLoaded",function()
           fetch("http://localhost:7777/video-request",{method :"POST"
           ,
           body:form_Data 
-        }).then((data)=>
+        }).then(bold=>bold.json()).then((data)=>
         {
-           listofconatiers.ap(getdata(data))
+            getdata(data,true);
         })
         })
-      })
+      });
       
